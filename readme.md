43ae9d56e91e2b7e2611353b893c6eccd3fe7776
denalse <denaleewonders@gmail.com>
2024-02-08 09:43:46 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+## Download Maven
+
+```
+mvn archetype:generate -DgroupId=nus.iss -DartifactId=task01 -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
+```
+
+*use DataInputStream (dis) to read the file*
+*use DataOutputStream (dos) to write the file*
---
+++ task01/src/test/java/nus/iss/AppTest.java
@@ -0,0 +1,32 @@
+package nus.iss;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    //@Test
+    // public void listAllUsers() {
+    //     ShoppingCartDB db = new ShoppingCartDB("cartdb");
+    //     List<String> users = db.listUsers(); //create method in shoppingcartdb
+    //     System.out.println(!users.isEmpty());
+
+    //     if (!users.isEmpty()) { 
+    //         System.out.println("not empty");
+    //     }
+    //     assertTrue(!users.isEmpty());
+    //}
+}
